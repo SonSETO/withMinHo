56b5dbe11e6c4447e7266c6fac69cc2d7d911e4a
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-09-27 10:00:59 +0900

    Test : cors 수정

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,11 @@ dbConnect();
 app.use(cookieParser());
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript with Express!");
